a1c545ce66c540b6cfa9e3b1a96089227c586276
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-04-26 20:55:28 +0430

    added HomeRepository

--- app/src/main/java/com/jmzd/ghazal/foodappmvi/data/repository/HomeRepository.kt
+++ app/src/main/java/com/jmzd/ghazal/foodappmvi/data/repository/HomeRepository.kt
@@ -1,21 +1,12 @@
 package com.jmzd.ghazal.foodappmvi.data.repository
 
-import android.util.Log
-import com.jmzd.ghazal.foodappmvi.data.model.home.ResponseCategoriesList
-import com.jmzd.ghazal.foodappmvi.data.model.home.ResponseFoodsList
 import com.jmzd.ghazal.foodappmvi.data.server.ApiServices
-import com.jmzd.ghazal.foodappmvi.utils.MyResponse
-import com.jmzd.ghazal.foodappmvi.utils.TAG
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import retrofit2.Response
 import javax.inject.Inject
 
-
-/*فقط api رندوم را ا حالت عادی میزنیم بقیه api ها را با حالت myResponse میزنیم که اصولی تر است */
 class HomeRepository @Inject constructor(private val api : ApiServices) {
-
+    suspend fun randomFood() = api.foodRandom()
+    suspend fun categoriesList() = api.categoriesList()
+    suspend fun foodsList(letter: String) = api.foodsList(letter)
+    suspend fun searchFood(letter: String) = api.searchFood(letter)
+    suspend fun foodByCategory(letter: String) = api.foodsByCategory(letter)
 }
